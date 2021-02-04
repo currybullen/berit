424cdf15209c332f8e3e32019218d4d5b6792e0a
Mikael Karlsson <mikaelhakarlsson@gmail.com>
2021-02-04 22:56:18 +0100

    Replace messy list comprehension with simple loop

--- berit.py
+++ berit.py
@@ -74,7 +74,12 @@ def start_discord_listener(cards, api_key):
             logging.debug(f"No patterns could be extracted from message '{message.content}'.")
             return
 
-        matches = [card for card in (find_card(cards, pattern) for pattern in patterns) if card is not None]
+        matches = []
+        for pattern in patterns:
+            match = find_card(cards, pattern)
+            if match is not None:
+                matches.append(match)
+
         if not matches:
             logging.debug(f"No card(s) found matching patterns '{patterns}'.")
             return
